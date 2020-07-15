0278231e940b2a2d64bcf71b06483838c0934e88
Dori <email>
2020-07-15 21:48:25 +0300

    Format the list of nodes objects into a dict

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json, node
+import json
+
+import node
 
 with open("data/nodes.json") as json_file:
     json_file = json_file.read()
     nodes = json.loads(json_file, object_hook=node.decode_json_to_nodes)
+
+nodes = {node.id:node.edges for node in nodes}
